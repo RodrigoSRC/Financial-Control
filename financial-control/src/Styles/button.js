b26b04af85a61821321c5775fdba3808eecd13cc
Rodrigo Simões Ribeiro Costa <clancoto2015@hotmail.com>
2024-01-05 09:53:37 -0300

    styled: tamanho da fonte atualizado

--- financial-control/src/Styles/button.js
+++ financial-control/src/Styles/button.js
@@ -4,6 +4,8 @@ export const StyledButtonForm = styled.button`
     font-family: var(--Font-Primary);
     height: 48px;
     width: 100%;
+    font-size: 17px;
+    
 
     color: var(--color-grey-1);
     background: var(--color-color-primary);
